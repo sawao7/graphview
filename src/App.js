5507f82c301dd2a935e9e9efe7c6dbb3bcf2c87b
澤田悠太 <yuta2929ayax@gmail.com>
2022-04-19 18:49:23 +0900

    succeed view all year and value one prefecture

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import axios from "axios";
 
 function App() {
 	const [data, setData] = React.useState(null);
+	const [all, setAll] = React.useState(null);
 
 	React.useEffect(() => {
 		axios
-			.get(
-				"https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11",
-				{
-					headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
-				}
-			)
+			.get("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=11", {
+				headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
+			})
 			.then((response) => {
-				// setData(response.json().result);
 				setData(response.data);
 			});
 	}, []);
 
+	React.useEffect(() => {
+		axios
+			.get("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+				headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
+			})
+			.then((response) => {
+				setAll(response.data);
+			});
+	}, []);
+
 	if (!data) return null;
-	console.log(data);
+	if (!all) return null;
+
+	// allには、すべての都道府県とIDが格納 all.result[].prefCode/PrefName
+	// dataには、表示すべきグラフの内容
+	// data.result.data[0]に総人口の推移、その中のdataのyearとvalueあり
+	// const lists = data.result.data[0].data.length;
 
 	return (
 		<div className="App">
@@ -30,7 +42,18 @@ function App() {
 				<p>
 					<code>src/App.js</code> and save to reload.
 				</p>
-				<p>{data.result.boundaryYear}</p>
+				{/* <p>{data.result.boundaryYear}</p> */}
+				{data.result.data[0].data.map((element) => {
+					return (
+						<p>
+							{element.value}, {element.year}
+						</p>
+					);
+				})}
+				{/* <p>{data.result.data[0].data[10].value}</p> */}
+				{all.result.map((data) => {
+					return <li>{data.prefName}</li>;
+				})}
 
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
