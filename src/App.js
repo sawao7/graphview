937fbc27ccf24695e7a53cdb814f3bc8158c2449
澤田悠太 <yuta2929ayax@gmail.com>
2022-04-22 07:06:19 +0900

    done view graph with dynamic

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ import HighchartsReact from "highcharts-react-official";
 function App() {
 	const [data, setData] = React.useState(null);
 	const [all, setAll] = React.useState(null);
+	const [num, setNum] = React.useState(0);
+
+	// const prefNum = 0;
 
 	React.useEffect(() => {
 		axios
-			.get("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=11", {
+			.get(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${num}`, {
 				headers: { "X-API-KEY": process.env.REACT_APP_API_KEY },
 			})
 			.then((response) => {
 				setData(response.data);
 			});
-	}, []);
+	}, [num]);
 
 	React.useEffect(() => {
 		axios
@@ -29,7 +32,6 @@ function App() {
 			});
 	}, []);
 
-
 	if (!data) return null;
 	if (!all) return null;
 
@@ -40,12 +42,37 @@ function App() {
 	// const lists_demo = [[20, 1]];
 
 	// リストに
-	const lists_demo =  [
-		{
-			name: "test",
-			data: [10,11,12,3,20],
-		},
-	];
+
+	// const prefData = [data.result.data[0].data[0].value, data.result.data[0].data[1].value];
+	const prefData = [];
+	for (let i = 0; i < data.result.data[0].data.length; i++) {
+		prefData.push(data.result.data[0].data[i].value);
+	}
+	// const prefData = [2,3,4,5]
+	const prefName = all.result[num].prefName;
+	const series = [];
+	const serie = {
+		name: prefName,
+		data: prefData,
+	};
+	series.push(serie);
+	// const lists_demo = [
+	// 	{
+	// 		name: "test",
+	// 		data: [
+	// 			10, 11, 12, 3, 20,
+	// 			// [1, 2, 2, 3, 4],
+	// 		],
+	// 	},
+	// 	{
+	// 		name: "test2",
+	// 		data: [10, 2,3,45,6]
+	// 	}
+	// ];
+	// lists_demo.push({
+	// 	name: "test3",
+	// 	data:[2,34,5,6,7]
+	// })
 
 	const options = {
 		title: {
@@ -57,27 +84,43 @@ function App() {
 					connectorAllowed: false,
 				},
 				pointInterval: 5,
-				pointStart: 1965,
+				pointStart: 1960,
 			},
 		},
-		series: lists_demo,
+		series: series,
 	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
+				{all.result.map((prefecture) => {
+					return (
+						<div>
+							<input
+								type="checkbox"
+								name="Prefecture name"
+								value="test"
+								onChange={(event) => {
+									setNum(prefecture.prefCode - 1);
+								}}
+							></input>
+							<lebel>{prefecture.prefName}</lebel>
+						</div>
+					);
+				})}
+
 				{/* <p>{data.result.boundaryYear}</p> */}
-				{data.result.data[0].data.map((element) => {
+				{/* {data.result.data[0].data.map((element) => {
 					return (
 						<p>
 							{element.value}, {element.year}
 						</p>
 					);
 				})}
-				{/* <p>{data.result.data[0].data[10].value}</p> */}
+				<p>{data.result.data[0].data[10].value}</p>
 				{all.result.map((data) => {
 					return <li>{data.prefName}</li>;
-				})}
+				})} */}
 
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
